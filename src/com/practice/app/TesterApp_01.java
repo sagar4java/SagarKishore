3cd03f2a8d4fbd24ad02d1f899b4d0130d4e6d89
Kishor Kumar <kishorgn@gmail.com>
2020-06-20 13:08:59 +0530

    Commit - 2 by kishor

--- src/com/practice/app/TesterApp_01.java
+++ src/com/practice/app/TesterApp_01.java
@@ -10,6 +10,11 @@ public class TesterApp_01 {
 		// Added by kishor
 		Employee ki01 = new Employee(101, "Kishor", 501);
 		System.out.println("existing : "+ki01);
+		
+		// Modification by Kishor after Sagar's branch - 1
+		
+		Employee ki02 = new Employee(104, "Kumar", 502);
+		System.out.println("newly added "+ki02);
 
 	}
 
---
+++ src/com/practice/app/TesterApp_02.java
@@ -0,0 +1,12 @@
+package com.practice.app;
+
+public class TesterApp_02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Newly craeted program");
+
+	}
+
+}
