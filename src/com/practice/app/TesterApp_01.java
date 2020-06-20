c4b8a0bf9f142232c25b85190227fe24d8df5732
Kishor Kumar <kishorgn@gmail.com>
2020-06-20 11:28:04 +0530

    Commit - 01: by kishor, with single employee create and display.

---
+++ src/com/practice/app/TesterApp_01.java
@@ -0,0 +1,16 @@
+package com.practice.app;
+
+import com.practice.bean.Employee;
+
+public class TesterApp_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Added by kishor
+		Employee ki01 = new Employee(101, "Kishor", 501);
+		System.out.println("existing : "+ki01);
+
+	}
+
+}
---
+++ src/com/practice/bean/Employee.java
@@ -0,0 +1,42 @@
+package com.practice.bean;
+
+public class Employee {
+	
+	int id;
+	String name;
+	int deptno;
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Employee(int id, String name, int deptno) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.deptno = deptno;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getDeptno() {
+		return deptno;
+	}
+	public void setDeptno(int deptno) {
+		this.deptno = deptno;
+	}
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", deptno=" + deptno + "]";
+	}
+	
+
+}
