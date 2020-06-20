92eb5b8ae4b077eb07d4565fbd46f900ef725f75
javatechie.sagar@gmail.com <javatechie.sagar@gmail.com>
2020-06-20 12:29:59 +0530

    Branch-1 Created By Sagar
    New Employee object sagar has been created!

---
+++ DemoProj_01/src/com/practice/app/TesterApp_01.java
@@ -0,0 +1,19 @@
+package com.practice.app;
+
+import com.practice.bean.Employee;
+
+public class TesterApp_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Added by kishor
+		Employee ki01 = new Employee(101, "Kishor", 501);
+		System.out.println("existing : " + ki01);
+		// Added by Sagar
+		Employee sagar = new Employee(102, "Sagar", 502);
+		System.out.println("existing : " + sagar);
+
+	}
+
+}
